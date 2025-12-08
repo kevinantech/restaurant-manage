61d1b72e7e2c8e7a4bc7364d785fce2015507270
Kevin Andres <kevinandresgom19@gmail.com>
2025-12-07 20:30:57 -0500

    fix: adjust margin of back button

--- app/_components/pages/RegisterPage/BackButton.tsx
+++ app/_components/pages/RegisterPage/BackButton.tsx
@@ -8,7 +8,7 @@ const BackButton: React.FC<BackButtonProps> = ({}) => {
   return (
     <Link
       href={WebRoutes.LOGIN}
-      className="absolute m-8 flex items-center w-40 p-2 rounded-full bg-[#661a5e] shadow-sm transition-all ease-in-out duration-200 hover:bg-primary hover:shadow-lg hover:translate-y-[-1px]"
+      className="absolute m-8 mt-4 md:mt-8 flex items-center w-40 p-2 rounded-full bg-[#661a5e] shadow-sm transition-all ease-in-out duration-200 hover:bg-primary hover:shadow-lg hover:translate-y-[-1px]"
     >
       <div className="flex-1 flex">
         <ArrowBackIosRoundedIcon fontSize="small" className="text-white" />
